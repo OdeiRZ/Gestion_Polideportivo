96ba6266e37e975be9a8df3cfa7b4da92a2bf485
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2017-06-01 12:31:45 +0200

    Inclusión de librerías externas

---
+++ Gestion_Polideportivo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <time.h>
+#define FICHERO1 "Reservas.dat"
+#define FICHERO2 "Clientes.dat"
+#define FICHERO3 "Instalacion.dat"
+#define FICHERO4 "Facturas.dat"
+
